951bb3bbbf85fb0b2664fbc1b7396fab5c31d06b
martin70m <martin70m@gmail.com>
2026-04-17 15:38:24 +0200

    post beer

--- src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
@@ -4,10 +4,9 @@ import guru.springframework.spring7restmvc.services.BeerService;
 import guru.springframework.spring7restmvc.model.Beer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -22,6 +21,15 @@ import java.util.UUID;
 public class BeerController {
     private final BeerService beerService;
 
+    @PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity handlePost(@RequestBody Beer beer) {
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
@@ -30,7 +38,7 @@ public class BeerController {
     @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
-        log.debug("Get Beer by Id - in controller");
+        log.debug("Get Beer by Id - in controller - 1234 asdfadsf");
 
         return beerService.getBeerById(beerId);
     }
